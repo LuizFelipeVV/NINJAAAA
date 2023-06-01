e756ca77bda32bbf8d182079029272f65d33e3ce
LuizFelipeVV <113483567+LuizFelipeVV@users.noreply.github.com>
2023-06-01 19:55:33 -0300

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -4,16 +4,28 @@ from PPlay.gameimage import *
 from PPlay.mouse import *
 from PPlay.keyboard import *
 
+def game():
+   
+    global janela
+    global fundo1
+    global fundo2
+   
+    while True:
+
+    
+    
+        fundo2.set_position(-300,200)
+        fundo1.draw()
+        fundo2.draw()
+        janela.update()
+
+    return 0
+
+
 
 
 janela = Window(1200,700)
 fundo1 = GameImage("estrelas.png")
 fundo2 = GameImage("bloco2.png")
-while True:
 
-    
-    
-    fundo2.set_position(-300,200)
-    fundo1.draw()
-    fundo2.draw()
-    janela.update()
+game()
